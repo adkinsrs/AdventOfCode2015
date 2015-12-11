267be996864371303e4108e388a968e204a1aa35
Shaun Adkins <shaunadkins@macintosh.home>
2015-12-11 08:09:03 -0500

    updating Dec 11 progress

--- 12_11.py
+++ 12_11.py
@@ -1,20 +1,23 @@
 import sys
 
 bad_letters = set(['i', 'o', 'l'])
-good_letters = 'abcdefghjkmnpqrstuvwxyz'
+letter_max = ord('z')
 
-def main():
-	input = 'vzbxkghb'
-	letter_min = ord('a')
-	letter_max = ord('z')
-	while True:
+def increment_letter(input, index):
 		c_num = ord(input[-1])
 		# Increment again if incremented letter is in 'bad_letters'
 		if chr(c_num + 1) in bad_leters:
 			input[-1] = chr(c_num + 2)
-		# If new char is beyond 'z', reset to 'a'
+		# If new char is beyond 'z', reset to 'a' and go to next letter
 		elif c_num + 1 > letter_max:
 			input[-1] = 'a'
+			increment_letter(input, index-1)
+		return input
+
+def main():
+	string = 'vzbxkghb'
+	while True:
+		new_string = increment_letter(string, -1)
 
 if __name__ == '__main__':
 	main()
